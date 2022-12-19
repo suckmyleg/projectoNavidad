eda2940b819cf3cabc2444db9883094774946d9d
SuckMyLeg <juanito-146@hotmail.es>
2022-12-19 15:40:34 +0100

    Saving

---
+++ js/base.js
@@ -0,0 +1,6 @@
+function displayNav(){
+	var st = document.getElementById("navegacion_list").style;
+	var nv = document.getElementById("navegacion").style;
+	st.display = "block";
+	nv.height = "100%";
+}
---
+++ js/setup.js
@@ -0,0 +1,8 @@
+onload = function() {
+	console.log("Setup");
+	
+	if (window.location.href != "file:///C:/Users/juani/Desktop/ProyectoNavidad/index.html"){
+		document.getElementById("page_title").style.display = "none";
+	}
+}
+
