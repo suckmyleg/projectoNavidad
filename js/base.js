68430837cbf0ff4b553b2f2dcb3bd3de03a2e70b
suckmyleg <juanito-146@hotmail.es>
2022-12-20 15:36:55 +0100

    Saving

--- js/base.js
+++ js/base.js
@@ -1,6 +1,15 @@
 function displayNav(){
 	var st = document.getElementById("navegacion_list").style;
 	var nv = document.getElementById("navegacion").style;
-	st.display = "block";
-	nv.height = "100%";
+	var sw = document.getElementById('buttonDisplayer');
+
+	if(st.display != "block"){
+		st.display = "block";
+		sw.innerHTML = "---";
+
+	}else{
+		st.display = "none";
+		sw.innerHTML = "+++";
+
+	}
 }
--- js/setup.js
+++ js/setup.js
@@ -1,8 +1,50 @@
 onload = function() {
 	console.log("Setup");
 	
-	if (window.location.href != "file:///C:/Users/juani/Desktop/ProyectoNavidad/index.html"){
+	if (false && window.location.href != "file:///C:/Users/juani/Desktop/projectoNavidad/index.html"){
 		document.getElementById("page_title").style.display = "none";
 	}
+
+	bannerChangeColor();
 }
 
+function getRandomNumber(limit){
+	return Math.floor(Math.random()*limit)
+}
+
+function getRandomColor(){
+	var h = getRandomNumber(360);
+	var s = getRandomNumber(200);
+  	var l = getRandomNumber(70);
+
+	return `hsl(${h}deg, ${s}%, ${l}%)`;
+}
+
+let isMouseOver = false;
+let modoFiesta = false;
+
+function bannerChangeColor() {
+	console.log("Setup");
+
+	new_color = getRandomColor();
+
+	pageTitle = document.getElementById('page_title');
+
+	if(modoFiesta){
+		pageTitle.style.backgroundColor = new_color;
+		//pageTitle.innerHTML = "SuckMyLeg ("+new_color+")";
+	}
+	setTimeout(function() {bannerChangeColor();}, 700);
+}
+
+function switchModoFiest() {
+	modoFiesta = !modoFiesta;
+}
+
+function onMouseOverBanner() {
+	isMouseOver = true;
+}
+
+function onMouseOutBanner(){
+	isMouseOver = false;
+}
