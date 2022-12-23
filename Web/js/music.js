c3ffe9017f7444795e103d02703b5268b1fccb1f
SuckMyLeg <juanito-146@hotmail.es>
2022-12-23 14:08:48 +0100

    Saving

--- Web/js/music.js
+++ Web/js/music.js
@@ -13,7 +13,7 @@ function musicPlayingElement(){
 
 	let add = new Date(totalPlaying * 1000).toISOString().substring(14, 19)
 
-	return "<audio controls autoplay id='musicPlayer'>"+
+	return "<audio controls autoplay>"+
 	"<source src='Music/"+MUSICPLAYING+".mp3#t=00:"+add+"' type='audio/mpeg'>"+
 	"<source src='../Music/"+MUSICPLAYING+".mp3#t=00:"+add+"' type='audio/mpeg'>"+
 	"Your browser does not support the audio element"+
@@ -26,7 +26,8 @@ function showMusicPlaying(){
 
 function continuePlaying(){
 	showMusicPlaying();
-	document.body.innerHTML += musicPlayingElement();
+	document.getElementById("audioPlayer").innerHTML = musicPlayingElement();
+	document.getElementById("musicPlayerTitle").innerHTML = "Playing: "+MUSICPLAYING;
 }
 
 function playMusic(song){
@@ -38,13 +39,22 @@ function playMusic(song){
 	continuePlaying();
 }
 
+function setupAudioPlayer(){
+	document.body.innerHTML += "<div id='audioPlayer'></div>";
+}
+
 function showPlayer(){
-	document.body.innerHTML += "<a onclick='playMusic("+
-	'"Carol of The Bells - EPIC VERSION"'+
-	")'>Play music</a>";
+	document.body.innerHTML += "<div id='musicPlayer'>"+
+	"<p id='musicPlayerTitle'>Player</p><ul>"+
+	"<a onclick='playMusic("+'"Carol of The Bells - EPIC VERSION"'+")'>Carol of The Bells</a>"+
+	"<a onclick='playMusic("+'"Carol of The Bells - EPIC VERSION"'+")'>Carol of The Bells</a>"+
+	"</ul>"+
+	"</div>";
 }
 
 function musicStart(){
+
+	setupAudioPlayer();
 
 	if (MUSICPLAYING){
 		continuePlaying();
