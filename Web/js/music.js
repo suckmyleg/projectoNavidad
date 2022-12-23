fd55c0e2cb017743c81e46c7dd63dcb5372d5a6a
SuckMyLeg <juanito-146@hotmail.es>
2022-12-23 13:44:43 +0100

    Saving

--- Web/js/music.js
+++ Web/js/music.js
@@ -1,5 +1,7 @@
 let MUSICPLAYING = getCookie("MUSICPLAYING");
 let SINCEPLAYING = getCookie("SINCEPLAYING");
+let PAUSED = getCookie("PAUSED");
+let PAUSEDAT = getCookie("PAUSEDAT");
 
 function getTimestampInSeconds () {
   return Math.floor(Date.now() / 1000);
@@ -11,8 +13,9 @@ function musicPlayingElement(){
 
 	let add = new Date(totalPlaying * 1000).toISOString().substring(14, 19)
 
-	return "<audio controls autoplay>"+
+	return "<audio controls autoplay id='musicPlayer'>"+
 	"<source src='Music/"+MUSICPLAYING+".mp3#t=00:"+add+"' type='audio/mpeg'>"+
+	"<source src='../Music/"+MUSICPLAYING+".mp3#t=00:"+add+"' type='audio/mpeg'>"+
 	"Your browser does not support the audio element"+
 	"</audio>";
 }
