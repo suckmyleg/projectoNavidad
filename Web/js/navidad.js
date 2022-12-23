8a383816f32391897c4288f4527bebe8bc1a844a
SuckMyLeg <juanito-146@hotmail.es>
2022-12-23 20:22:42 +0100

    Saving

--- Web/js/navidad.js
+++ Web/js/navidad.js
@@ -1,19 +1,30 @@
 //onPlaying("*", function(songName){$(document).snowfall.clear();})
 
+let autoRemoveSnowDelay = 500;
+let autoRemoveSnowRange = 100;
+
+function autoRemoveSnow(){
+	
+	for(var i = 0; i < autoRemoveSnowRange; i++){
+		try{
+			document.getElementById("flake-"+i).remove();
+		}
+		catch{}
+	}
+	
+	setTimeout(function(){autoRemoveSnow();}, autoRemoveSnowDelay);
+}
+
 onPlaying("*", function(songName){
 	var title = document.getElementById("page_title");
 	//title.innerHTML = songName;
 	$(document).snowfall().stop()
-	try{
-		for(var i = 0; i < 300; i++){
-			document.getElementById("flake-"+i).remove();
-		}
-	}
-	catch{}
+	autoRemoveSnowRange = 500;
 	title.style.backgroundImage = "none";})
 
 
 function navidad() {
+	autoRemoveSnowRange = 0;
 	$(document).snowfall({deviceorientation : true, round : true, minSize: 0.5, maxSize:8,  flakeCount : 150});
 }
 
@@ -41,3 +52,4 @@ onPlaying("Porta - Dragon Ball Rap (con Letra)", function(songName){
 	document.getElementById("page_title").style.backgroundImage = "url('../Images/Ruben.png')";
 })
 
+autoRemoveSnow();
