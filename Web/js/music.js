f425af221a04ddb1b4d828a6b4d6b8e5e9f38d7f
SuckMyLeg <juanito-146@hotmail.es>
2022-12-24 04:00:51 +0100

    Saving

--- Web/js/music.js
+++ Web/js/music.js
@@ -215,8 +215,8 @@ function getSettings() {
 	'<a onclick="'+makeChange("cinemaheight", "76px")+'">Normal</a>'+
 	'<a onclick="'+makeChange("cinemaheight", "200px")+'">Short</a>'+
 	'<a onclick="'+makeChange("cinemaheight", "600px")+'">Medium</a>'+
-	'<a onclick="'+makeChange("cinemaheight", "1000px")+'">Big</a>'+
-	'<a onclick="'+makeChange("cinemaheight", "100%")+'">Full</a>'+
+	'<a onclick="'+makeChange("cinemaheight", "1000px")+'">Full</a>'+
+	'<a onclick="'+makeChange("cinemaheight", "100%")+'">Background</a>'+
 	"</div>";
 }
 
