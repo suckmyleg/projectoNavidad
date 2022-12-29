eaad21f90e21e0f718769abb72b0f11bf11f7b67
SuckMyLeg <juanito-146@hotmail.es>
2022-12-30 00:41:49 +0100

    Saving

--- Web/js/camara.js
+++ Web/js/camara.js
@@ -1,6 +1,16 @@
 
+let HOST = window.location.host.split(":")[0];
+
+function switchFlash(){
+	makeCallFalse("http://"+HOST+":5000/MobileSwitchFlash");
+}
+
+function makeCallFalse(){
+	document.getElementById("spawnableField").innerHTML = "<iframe class='hidden' src='"+src+"'></iframe>";
+}
+
 ONLOADS.push(function(){
 
-document.getElementById("camera").innerHTML = "<img src='http://"+window.location.host.split(":")[0]+":5000/video_feed' width='100%'>"
+document.getElementById("camera").innerHTML = "<img src='http://"+HOST+":5000/video_feed' width='100%'>";
 
 });
